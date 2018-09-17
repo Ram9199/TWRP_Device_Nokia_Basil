d6979bcc0bdfc07f55af791d5aef5884c1f5e03b
engstk <eng.stk@sapo.pt>
2018-09-17 13:09:44 +0100

    Basil:Add some cmdline fixes
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: leodiram <sriramscience7@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 BOARD_NAME := Qualcomm
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3 
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE += skip_override androidboot.fastboot=1
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE    := 4096
